9eefcf097f843885cfd3e851bf440982f4f96e59
Thalles Henrique <thalleshenrique7534@gmail.com>
2022-11-18 10:30:10 -0300

    adding kata description

--- 6kyu/CamelCase Method.php
+++ 6kyu/CamelCase Method.php
@@ -1,4 +1,10 @@
 <?php
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+// For instance:
+// camel_case("hello case"); // => "HelloCase"
+// camel_case("camel case word"); // => "CamelCaseWord"
+// Don't forget to rate this kata! Thanks :)
+
 function camel_case(string $s): string {
     $upperCase = ucwords($s);
     return str_replace(' ', '', $upperCase);
